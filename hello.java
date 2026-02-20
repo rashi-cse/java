007d6209aaa4f13d3b82e8112d1188e936b64541
Rashi <cse.rashii@gmail.com>
2026-02-20 09:29:17 +0530

    Initial Java commit

---
+++ hello.java
@@ -0,0 +1,11 @@
+public class hello {
+  public static void main(String[] args) {
+    System.out.println("hello");
+    int a = 10;
+    int b = 20;
+    System.out.println(a + b);
+    System.out.println(a-b);
+    System.out.println(a*b);
+    System.out.println(a/b);
+  }
+}
